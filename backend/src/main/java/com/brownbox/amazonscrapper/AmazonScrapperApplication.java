fe44dda736d62065c3788497bbba94fcf02a6f7d
Cong <samuelmc1015@gmail.com>
2020-06-23 09:53:52 -0400

    initial commit

---
+++ backend/src/main/java/com/brownbox/amazonscrapper/AmazonScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.brownbox.amazonscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonScrapperApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brownbox/amazonscrapper/AmazonScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brownbox.amazonscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
